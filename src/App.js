48c5266f4341c0a55db003c970585cd005b1f5a7
shakirshah30480 <shakirshah30480@gmail.com>
2023-05-30 11:16:29 +0500

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect,useState } from "react";
+
+
+
+
 
 function App() {
+  const[num,setNum]=useState(0)
+
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products/0000000').then( (data)=>{
+      return data.json();
+    }).then( (res)=>{
+        console.log(res)
+       
+    }).catch( (err)=>{
+        console.log(err)
+    })
+  }, [])
+
+
+
+  fetch().then( () =>{}).then( ()=>{} ).catch( ()=>{ })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{num}</h1>
+      <button onClick={ ()=>setNum(num +1)}>plus</button>
     </div>
   );
 }
